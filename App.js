d3a7130762e1cc130105113788c0ad68ec30ba83
Krishna-Janaswamy <125383506+Krishna-Janaswamy@users.noreply.github.com>
2023-08-10 13:34:51 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading = React.createElement("h1", {}, "Hello world react");
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { key: 1, id: "child1" },
+    React.createElement("h1", { id: "child-h1" }, "Hello world react")
+  ),
+  React.createElement(
+    "div",
+    { key: 2, id: "child2" },
+    React.createElement("h1", { id: "child-h2" }, "Hello world react")
+  ),
+]);
+
+
+console.log(parent, "heading");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
