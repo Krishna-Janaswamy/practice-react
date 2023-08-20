bac06a6204b3e34d8359a47651a2f95e296398bd
Krishna-Janaswamy <125383506+Krishna-Janaswamy@users.noreply.github.com>
2023-08-20 16:09:57 +0530

    second commit

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {}, "Hello world react");
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
     { key: 1, id: "child1" },
-    React.createElement("h1", { id: "child-h1" }, "Hello world react")
+    React.createElement("h1", { id: "child-h1" }, "Hello world react 🤘")
   ),
-  React.createElement(
-    "div",
-    { key: 2, id: "child2" },
-    React.createElement("h1", { id: "child-h2" }, "Hello world react")
-  ),
+  // React.createElement(
+  //   "div",
+  //   { key: 2, id: "child2" },
+  //   React.createElement("h1", { id: "child-h2" }, "Hello world react")
+  // ),
 ]);
 
 
---
+++ README.md
@@ -0,0 +1,20 @@
+
+
+# parcel (https//:www.parceljs.org)
+
+- Dev Built
+- Local Server
+- HMR = Hot Module Replacement
+- File watching Algorithm - written in c++
+- Caching - faster Builds
+- Image optimization
+- Minification in production mode (by bundling)
+- compresses code
+- Consistence hashing(need to be read)
+- code splitting
+- differential bundling
+- Diagnostic 
+- Error handling
+- supports Https
+- Tree shaking
+- different bundle for dev and prod
